4e8f4535ab159658579755c4f63d108dec9e16f0
Serkan <skaya0@hotmail.com>
2020-09-02 23:21:16 +0300

    Update init_pontesolo.cpp

--- init/init_pontesolo.cpp
+++ init/init_pontesolo.cpp
@@ -30,6 +30,7 @@
 #include <stdlib.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <cutil/properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -52,17 +53,17 @@ void vendor_load_properties()
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
         return;*/
 
-    android::init::property_get("ro.bootloader", bootloader);
+    property_get("ro.bootloader", bootloader);
 
    /* pontesolo */
-     android::init::property_set("ro.build.fingerprint", "samsung/pontesoloxx/pontesolo:4.4.2/KOT49H/I9301IXXUANL1:user/release-keys");
-     android::init::property_set("ro.build.description", "pontesoloxx-user 4.4.2 KOT49H I9301IXXUANL1 release-keys");
-     android::init::property_set("ro.product.model", "SM-R750");
-     android::init::property_set("ro.product.device", "pontesolo");
-     android::init::property_set("telephony.lteOnGsmDevice", "0");
-     android::init::property_set("ro.telephony.default_network", "0");
+     property_set("ro.build.fingerprint", "samsung/pontesoloxx/pontesolo:4.4.2/KOT49H/I9301IXXUANL1:user/release-keys");
+     property_set("ro.build.description", "pontesoloxx-user 4.4.2 KOT49H I9301IXXUANL1 release-keys");
+     property_set("ro.product.model", "SM-R750");
+     property_set("ro.product.device", "pontesolo");
+     property_set("telephony.lteOnGsmDevice", "0");
+     property_set("ro.telephony.default_network", "0");
     // property_set("ro.telephony.ril_class", "SamsungMSM8226RIL");
-    android::init::property_get("ro.product.device", device);
+    property_get("ro.product.device", device);
     strlcpy(devicename, device, sizeof(devicename));
     //INFO("Found bootloader id %s setting build properties for %s device\n", bootloader, devicename);
 }
