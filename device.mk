b0a33d59a0936c8e824e5c23fbfb027e7e851c90
Serkan <skaya0@hotmail.com>
2020-09-09 17:27:02 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/samsung/pontesolo/pontesolo-vendor.mk)
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_policy.xml:system/etc/audio_policy.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 
